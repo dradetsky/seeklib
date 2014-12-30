febe34679537d680a42a6b5e806947413a6d146b
Daniel Radetsky <dradetsky@gmail.com>
2014-12-29 23:12:30 -0800

    make many hashes at once

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 
 #include "haar.h"
 #include "sig.h"
@@ -13,8 +14,30 @@ int main(int argc, char** argv) {
         printf("no path");
         exit(0);
     }
-    SigStruct *sig = pathSig(argv[1]);
-    printSig(sig);
+
+    const char *dir_name = argv[1];
+    struct dirent *dir_entry;
+    DIR *dir = opendir(dir_name);
+    if (!dir){
+	printf("unable to open directory\n");
+	exit(1);
+    }
+    int count = 0;
+    char path[1024];
+    path[0] = '\0';
+    while ((dir_entry = readdir(dir)) != 0){
+	if (strcmp(dir_entry->d_name,".") && strcmp(dir_entry->d_name,"..")){
+	    strcat(path, dir_name);
+	    strcat(path, "/");
+	    strcat(path, dir_entry->d_name);
+            SigStruct *sig = pathSig(path);
+            printSig(sig);
+        }
+        path[0]='\0';
+        if (++count > 1000)
+            break;
+    }
+
     printf("ok\n");
     exit(0);
 }
